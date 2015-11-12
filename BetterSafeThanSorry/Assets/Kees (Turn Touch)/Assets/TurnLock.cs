4a40fd981a7606ebb2a62dbb5f28e9f59ee6a848
Werson,Kees K <k.werson@student.fontys.nl>
2015-11-12 15:06:00 +0100

    Rotate Lock
    Lock rotation via Touch

---
+++ BetterSafeThanSorry/Assets/Kees (Turn Touch)/Assets/TurnLock.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TurnLock : MonoBehaviour {
+
+    //rotationspeed
+    public float speed;
+    private float x;
+    private float y;
+    //Debug variabeles:
+    public Text XValue;
+    public Text YValue;
+    public Text MousePositionText;
+    //Mouse-position
+    private Vector2 mousePosition;
+
+	// Use this for initialization
+	void Start () {
+        //If no speed is selected.
+        if (speed == 0)
+        {
+            speed = 5;
+        }
+
+        //getting mouse postion
+        mousePosition = Input.mousePosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+
+        if (Input.GetMouseButton(0))
+        {
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {             
+                //get the mouse-position value
+                x = Input.GetAxis("Mouse X");
+                y = Input.GetAxis("Mouse Y");
+
+                //getting position
+                mousePosition = Input.mousePosition;
+                //test cooordinates
+                drawValues();
+
+                //rotates the lock
+                //if the cursor is in the upper-right section
+                if((double)mousePosition.x > Screen.width * 0.5 && (double)mousePosition.y > Screen.height * 0.5){
+                    transform.rotation *= Quaternion.AngleAxis(x * speed, Vector3.back);
+                    transform.rotation *= Quaternion.AngleAxis(y * speed, Vector3.forward);
+                }
+                //if the cursor is in the bottom-right section
+                else if ((double)mousePosition.x > Screen.width * 0.5 && (double)mousePosition.y < Screen.height * 0.5){
+                    transform.rotation *= Quaternion.AngleAxis(x * speed, Vector3.forward);
+                    transform.rotation *= Quaternion.AngleAxis(y * speed, Vector3.forward);
+                }
+                //if the cursor is in the bottom-left section
+                else if ((double)mousePosition.x < Screen.width * 0.5 && (double)mousePosition.y < Screen.height * 0.5)
+                {
+                    transform.rotation *= Quaternion.AngleAxis(x * speed, Vector3.forward);
+                    transform.rotation *= Quaternion.AngleAxis(y * speed, Vector3.back);
+                }
+                //if the cursor is in the upper-left section
+                else if ((double)mousePosition.x < Screen.width * 0.5 && (double)mousePosition.y > Screen.height * 0.5)
+                {
+                    transform.rotation *= Quaternion.AngleAxis(x * speed, Vector3.back);
+                    transform.rotation *= Quaternion.AngleAxis(y * speed, Vector3.back);
+                }
+                
+            }
+        }
+	}
+
+    public void drawValues(){
+        XValue.text = "X: " + x;
+        YValue.text = "Y: " + y;
+        MousePositionText.text = "Mouse Position: " + mousePosition.x + ";" + mousePosition.y;
+    }
+}
