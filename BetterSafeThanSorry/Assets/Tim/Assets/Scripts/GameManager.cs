eded9f1831ceda5ddd0e9dd1d136e02650fbe581
TimOomis <t.oomis@student.fontys.nl>
2015-11-12 13:47:12 +0100

    UICommitV1
    Start menu, ingamescreen (+ fade) met pauze en gameover screen.
    Momenteel alleen basis.

---
+++ BetterSafeThanSorry/Assets/Tim/Assets/Scripts/GameManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject gameplayObject;
+	public GameObject eventPanel;
+	bool canMove = false; 
+
+	Vector3 startPos = Vector3.zero;
+	Vector3 endPos; 
+	public float speed = 15F;
+
+	// Use this for initialization
+	void Start () {
+		endPos = new Vector3 (15, 0, 0);
+
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			canMove = true;
+		}
+		if (canMove) 
+		{
+			StartCoroutine(moveInEventPanel());
+		}
+	}
+
+	IEnumerator moveInEventPanel()
+	{
+
+		float step = Time.deltaTime * speed;
+		gameplayObject.transform.position = Vector3.Lerp(gameplayObject.transform.position, endPos, step);		
+		yield return new WaitForSeconds (1f);
+		StartCoroutine (fadeEventPanel ());
+	}
+
+	IEnumerator fadeEventPanel()
+	{
+		float targetScale = 1f;
+		float growSpeed = 2f;
+		eventPanel.transform.localScale = Vector3.Lerp(eventPanel.transform.localScale, new Vector3(targetScale, targetScale, targetScale), Time.deltaTime*growSpeed);
+	
+		float progress = 0.0f;
+		CanvasGroup canvasGroup = eventPanel.GetComponent<CanvasGroup> ();
+		float rate = 1.0f / growSpeed;
+		
+		while (progress < 1.0) 
+		{
+			canvasGroup.alpha = Mathf.Lerp (0.0f, 1.0f, progress);
+			progress += rate * Time.deltaTime;
+
+		}
+
+
+		yield return null;
+	}
+
+
+ 	
+}
---
+++ BetterSafeThanSorry/Assets/Tim/Assets/Scripts/IngameInterface.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IngameInterface : MonoBehaviour {
+
+	GameObject pausedPanel;
+	// Use this for initialization
+	void Start () 
+	{
+		pausedPanel = this.transform.FindChild("PausedPanel").gameObject;
+	}
+
+	void Update()
+	{
+
+	
+
+		if (Input.GetKeyDown (KeyCode.G)) 
+		{
+			GameObject gameOverPanel = this.transform.FindChild("GameOverPanel").gameObject;
+			gameOverPanel.SetActive(!gameOverPanel.activeSelf);
+		}
+	}
+
+	public void NavigateToMainMenu()
+	{
+		Application.LoadLevel (0);
+	}
+
+	public void NavigateToIngame()
+	{
+		Application.LoadLevel (1);
+		Time.timeScale = 1f;
+	}
+
+	public void PauseCurrent()
+	{
+		pausedPanel.SetActive(true);
+		Time.timeScale = 0f;
+	}
+
+	public void Resume()
+	{
+		pausedPanel.SetActive(false);
+		Time.timeScale = 1f;
+	}
+	
+
+}
